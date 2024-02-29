87c1338ff06dd70ae14c7885f20c49a8c1a0abd3
Tobin <w0473744@campus.nscc.ca>
2024-02-29 11:21:54 -0400

    Moved Logic to functions

--- py-src/testing.py
+++ py-src/testing.py
@@ -4,46 +4,88 @@
 
 import io
 import json
+
 from sys import argv
 from glob import glob
 from Lindex import lindex
 import zstandard as zstd
 
 
-FILE_EXTENSION = ".zst"
+def get_filenames() -> str:
+    """
+    Sets the filename to load the file from
+    """
 
-if __name__ == "__main__":
-
-
-    # Sets the filename to load the file from
-    try:
-        filename = argv[1]
-    except IndexError as e:
-        print(f"IndexError: {e}")
+    if len(argv) >= 2:
+        files = argv[1:]
+    else:
+        print(f"No Arguments Passed:")
         print(" - (You need to set a filename as a CLI argument!)")
-        print(f" - Defaulting to first `{FILE_EXTENSION}` file found in directory!")
+        print(f" - Defaulting to `{FILE_EXTENSION}` files found in directory!")
 
-        try:
-            filename = glob("*" + FILE_EXTENSION)[0]
-            print(f" - Found `{filename}`")
-        except IndexError as e:
+        files = glob("*" + FILE_EXTENSION)
+
+        if len(files) >= 1:
+            for file in files:
+                print(f"    - Found `{file}`")
+            print()
+
+        else:  # Exits the program
             print(f" - No file ending in `{FILE_EXTENSION}` FOUND! (exiting)")
             exit()  # Ends script execution
+    
+    return files
 
 
-    # Loads the file in (only sections of the file at a time)
+def load_file(filename: str, method):
+    """
+    Loads the file in (only sections of the file at a time)
+    """
+
     try:
-        with open(filename, "rb") as fh:
+        with open(filename, "rb") as fh:  # Loads file in
             dctx = zstd.ZstdDecompressor()
             stream_reader = dctx.stream_reader(fh)
             text_stream = io.TextIOWrapper(stream_reader, encoding="utf-8")
-            for line in text_stream:
-                print(line)
-                # obj = json.loads(line)
-                # lindex(obj).pprint()
-                input()
 
-    # Handles any exception
+            local_list = []
+            for line in text_stream:
+                # if line == "":
+                #     input(local_list)
+                #     local_list = []
+
+                method(line)
+
     except Exception as e:
         raise e
-        # print(f"Error: {e}")
+
+
+def handle_lines(line: str):
+    """
+    Defines the method for what to do to each line being passed in
+    """
+
+    global COUNT  # Uses a global line counter
+    global FILE_SPECIFIED
+
+    print(f" - File: {FILE_SPECIFIED} ~ Line: {COUNT}", end="\r")
+    COUNT += 1
+
+    # print(line.strip(), end="\r")
+    # obj = json.loads(line)
+    # lindex(obj).pprint()
+
+
+FILE_EXTENSION = ".zst"
+FILE_SPECIFIED = ""
+COUNT = 0
+
+
+if __name__ == "__main__":
+
+    files = get_filenames()
+    method = handle_lines
+    for i, filename in enumerate(files):
+        FILE_SPECIFIED = filename
+        load_file(filename=filename, method=method)
+        print()
