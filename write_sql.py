53fd401305d1ef8ddec75de8b7f58606411e1670
MT <04x0xx@gmail.com>
2024-03-15 15:13:32 -0300

    Added comments to write_sql.py

--- write_sql.py
+++ write_sql.py
@@ -7,10 +7,10 @@ Python file for making a SQL file for importing data
 from glob import glob
 from os import path
 
-directory = "data"
-table_name = "dbo.Games"
+directory = "data"  # The folder that holds the CSV files
+table_name = "dbo.Games"  # The database table to put the data into
 
-files = glob(path.join(directory, "*.csv"))
+files = glob(path.join(directory, "*.csv"))  # Gets a list of directories for the CSVs
 
 out_txt = [
 f"""
