5d7c069d561d0484cd7a4e3fbe35d850c4e27bfa
Tobin <W0473744@campus.nscc.ca>
2024-03-18 09:23:09 -0300

    Added Duplicate Data Prevention

--- write_sql.py
+++ write_sql.py
@@ -8,15 +8,24 @@ from glob import glob
 from os import path
 
 directory = "data"  # The folder that holds the CSV files
+data_file_meta = "dbo.Files"  # The name of the table the meta data for which files were added
 table_name = "dbo.Games"  # The database table to put the data into
 
 files = glob(path.join(directory, "*.csv"))  # Gets a list of directories for the CSVs
 
 out_txt = [
 f"""
-BULK INSERT {table_name}
-FROM '{path.abspath(file)}'
-WITH (FORMAT='CSV', FIRSTROW=2)
+IF NOT EXISTS (
+    SELECT * FROM {data_file_meta}
+    WHERE filename='{file}'
+)
+BEGIN
+    INSERT INTO {data_file_meta} (filename) VALUES ('{file}');
+
+    BULK INSERT {table_name}
+    FROM '{path.abspath(file)}'
+    WITH (FORMAT='CSV', FIRSTROW=2);
+END;
 
 GO
 """
