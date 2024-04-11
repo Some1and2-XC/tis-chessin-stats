569c56846cf9636c37e65c440919b5118d107937
Tobin <w0473744@campus.nscc.ca>
2024-04-11 11:14:00 -0300

    Added Visual Indicator for percentages

--- view/templates/chess-game.js
+++ view/templates/chess-game.js
@@ -8,6 +8,10 @@ var $eval = $("#eval");
 var $white_elo = $("#whiteElo");
 var $black_elo = $("#blackElo");
 
+var $white_res = $("#res-1-0");
+var $black_res = $("#res-0-1");
+var $draw_res = $("#res-draw");
+
 function onDragStart(source, piece, position, orientation) {
     if (game.game_over()) return false;
 
@@ -72,6 +76,19 @@ function updateStatus() {
         .then(res => res.json())
         .then(data => {
             $eval.html(data.eval);
+
+            const HEIGHT_FACTOR = 1;
+            const PROPERTY = "flex-grow"
+
+            $white_res.css(PROPERTY, data.prediction["1-0"] * HEIGHT_FACTOR);
+            $white_res.html(`<p>${(data.prediction["1-0"] * 100).toPrecision(2)}%</p>`);
+
+            $black_res.css(PROPERTY, data.prediction["0-1"] * HEIGHT_FACTOR);
+            $black_res.html(`<p>${(data.prediction["0-1"] * 100).toPrecision(2)}%</p>`);
+            $black_res.css("color", "white");
+
+            $draw_res.css(PROPERTY, data.prediction["1/2-1/2"] * HEIGHT_FACTOR);
+            $draw_res.html(`<p>${(data.prediction["1/2-1/2"] * 100).toPrecision(2)}%</p>`);
 
             return data;
         })
